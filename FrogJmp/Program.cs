9f600d8aa34b8b8dd59af4721ca77e4168b1f868
shiny13 <shahnawaz.alam13@gmail.com>
2024-07-14 03:07:13 +1000

    solution to frog jump problem

---
+++ FrogJmp/Program.cs
@@ -0,0 +1,20 @@
+﻿using FrogJmp;
+
+namespace FogJmp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Frog Jump!");
+        int X = 10, Y = 85, D = 30;
+
+        Console.WriteLine($"X: {X} Y: {Y} D: {D}");
+        var sol = new Solution();
+        var jumps = sol.Calculate(X, Y, D);
+
+        Console.WriteLine($"Jumps: {jumps}");
+
+        Console.ReadKey();
+    }
+}
---
+++ FrogJmp/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogJmp
+{
+    internal class Solution
+    {
+        public Solution() { }
+
+        public int Calculate(int X, int Y, int D)
+        {
+            // edge case for not needed to jump
+            if (X == Y)
+            {
+                return 0;
+            }
+
+            int goal = Y - X;
+            // edge case for 1 jump or a very large jump just once
+            if (D >= goal)
+            {
+                return 1;
+            }
+
+            int jumps = goal / D;
+            if (goal % D > 0)
+            {
+                jumps++;
+            }
+
+            return jumps;
+        }
+
+        // This solution will exceed the time limit, so no good!
+        public int Calculate2(int X, int Y, int D)
+        {
+            var jumps = 0;
+            if (X == Y)
+            {
+                return 0;
+            }
+
+            if (D >= Y - X)
+            {
+                return 1;
+            }
+
+            int position = X;
+            while (position < Y)
+            {
+                position = position + D;
+                jumps++;
+            }
+
+            return jumps;
+        }
+    }
+}
