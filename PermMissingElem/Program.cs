def43054fd7de46cfb56d2b5240a06f8d1d7bb5d
shiny13 <shahnawaz.alam13@gmail.com>
2024-07-14 03:26:06 +1000

    Solution for PermMissingElem

---
+++ PermMissingElem/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PermMissingElem;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("PermMissingElem");
+
+        var input = new int[] { 2, 3, 1, 5 };
+        foreach(var val in input)
+        {
+            Console.Write($"{val} ");
+        }
+        Console.WriteLine();
+
+        var sol = new Solution();
+        var missing = sol.Calculate(input);
+        Console.WriteLine($"missing: {missing}");
+        Console.ReadKey();
+    }
+}
---
+++ PermMissingElem/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermMissingElem
+{
+    internal class Solution
+    {
+        public Solution() { }
+
+        public int Calculate(int[] A)
+        {
+            if (A == null || A.Length == 0)
+            {
+                return 1;
+            }
+
+            int missing = -1;
+
+            var sorted = A.OrderBy(x => x).ToArray();
+            for (int i = 0; i < sorted.Length-1; i++)
+            {
+                if ((sorted[i+1] - sorted[i] > 1))
+                {
+                    missing = sorted[i] + 1; 
+                    break;
+                }
+            }
+
+            if (missing == -1 && sorted[0] > 1)
+            {
+                missing = sorted[0] - 1;
+            }
+            else if (missing == -1)
+            {
+                missing = sorted[sorted.Length - 1] + 1;
+            }
+
+            return missing;
+        }
+    }
+}
