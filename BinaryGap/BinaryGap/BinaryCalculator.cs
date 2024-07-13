016db9d217b08d065c571b80b3a2ef643df38f7e
shiny13 <shahnawaz.alam13@gmail.com>
2024-07-14 01:12:53 +1000

    Added BinaryGap project with readme

---
+++ BinaryGap/BinaryGap/BinaryCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.Metrics;
+using System.Linq;
+
+namespace BinaryGap;
+
+internal class BinaryCalculator
+{
+    public BinaryCalculator()
+    {
+    }
+
+    public int CalculateGap(int n)
+    {
+        var gap = new List<int>();
+        gap.Add(0);
+
+        string value = Convert.ToString(n, 2);
+        Console.WriteLine($"Input: {n} | Binary value: {value}");
+
+        var binaryArray = new List<char>();
+        var counter = 0;
+        foreach(char c in value)
+        {
+            binaryArray.Add(c);
+            Console.Write($"{binaryArray[counter]} ");
+            counter++;
+        }
+        Console.WriteLine();
+
+        int firstOne = -1;
+        int secondOne = -1;
+
+        for (int i = 0; i < binaryArray.Count; i++)
+        {
+            if (binaryArray[i] == '1' && firstOne == -1)
+            {
+                firstOne = i;
+                Console.WriteLine($"binary value: {binaryArray[i]} | firstOne: {firstOne}");
+            }
+            else if (binaryArray[i] == '1' && firstOne >= 0)
+            {
+                secondOne = i;
+                Console.WriteLine($"binary value: {binaryArray[i]} | secondOne: {secondOne}");
+                if ((secondOne - (firstOne+1)) > 0)
+                {
+                    gap.Add((secondOne - (firstOne + 1)));
+                }
+
+                firstOne = i;
+            }
+        }
+
+        return gap.Max();
+    }
+}
---
+++ BinaryGap/BinaryGap/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BinaryGap;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Binary Gap.");
+        var entries = new List<int>() { 1, 2, 9, 15, 128, 144, 145, 147, 483, 647 };
+
+        foreach (var entry in entries)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Calculating {entry} ...");
+            var calc = new BinaryCalculator();
+            var gapVal = calc.CalculateGap(entry);
+
+            Console.WriteLine($"Gap value: {gapVal}");
+            Console.WriteLine("---------------------------------------");
+        }
+
+        Console.ReadKey();
+    }
+}
