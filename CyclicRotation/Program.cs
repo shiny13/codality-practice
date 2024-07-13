0f0cd8d425e63bd180fa600a3ff271f07ba0946c
shiny13 <shahnawaz.alam13@gmail.com>
2024-07-14 02:07:13 +1000

    Cyclic rotation of array solution.

--- CyclicRotation/Program.cs
+++ CyclicRotation/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CyclicRotation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Cyclic Rotation");
+
+        var inputs = new List<int[]>()
+        {
+            new int[] { 3, 8, 9, 7, 6 },
+            new int[] { 6, 3, 8, 9, 7 },
+            new int[] { 7, 6, 3, 8, 9 }
+        };
+
+        foreach (var input in inputs)
+        {
+            var sol = new Solution();
+            var rotatedArr = sol.RotateArray(input, 1);
+
+            foreach (var val in rotatedArr) 
+            {
+                Console.Write($"{val} ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ CyclicRotation/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclicRotation;
+
+internal class Solution
+{
+    public Solution() { }
+
+    public int[] RotateArray(int[] A, int K)
+    {
+        if (A == null || A.Length == 0)
+        {
+            return new int[0];
+        }
+        else if (A.Length == K)
+        {
+            return A;
+        }
+        else if (K == 0)
+        {
+            return A;
+        }
+        else if (A.Length == 1)
+        {
+            return A;
+        }
+
+        if (K > A.Length)
+        {
+            K = K % A.Length;   
+        }
+
+        var rotatedArr = new int[A.Length];
+        for (int i = 0; i < A.Length; i++)
+        {
+            var rotatedIndex = i + K;
+            if (rotatedIndex < A.Length)
+            {
+                rotatedArr[rotatedIndex] = A[i];
+            }
+            else
+            {
+                rotatedIndex = rotatedIndex - A.Length;
+                rotatedArr[rotatedIndex] = A[i];
+            }
+        }
+
+        return rotatedArr;
+    }
+}
