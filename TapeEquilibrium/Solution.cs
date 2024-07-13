e33e26756ad3a1e57c2e4f08981a91b3a6ea0b96
shiny13 <shahnawaz.alam13@gmail.com>
2024-07-14 03:58:30 +1000

    solution to TapeEquilibrium

---
+++ TapeEquilibrium/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TapeEquilibrium
+{
+    internal class Solution
+    {
+        public Solution() { }
+
+        public int Calculate(int[] A)
+        {
+            int N = A.Length;
+
+            int sum1 = A[0];
+            int sum2 = 0;
+            int P = 1;
+            for (int i = P; i < N; i++)
+            {
+                sum2 += A[i];
+            }
+            int diff = Math.Abs(sum1 - sum2);
+
+            for (; P < N - 1; P++)
+            {
+                sum1 += A[P];
+                sum2 -= A[P];
+
+                int newDiff = Math.Abs(sum1 - sum2);
+                if (newDiff < diff)
+                {
+                    diff = newDiff;
+                }
+            }
+            return diff;
+        }
+    }
+}
