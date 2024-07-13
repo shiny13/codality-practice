53116e6d3341eaf1c710a31921e76e6db4051303
shiny13 <shahnawaz.alam13@gmail.com>
2024-07-14 02:32:27 +1000

    Solution for finding the odd occurence in an array for elements

--- OddOccurrencesInArray/Program.cs
+++ OddOccurrencesInArray/Program.cs
@@ -5,5 +5,18 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        var input = new int[] { 9, 3, 9, 3, 9, 7, 9 };
+        foreach (var val in input)
+        {
+            Console.Write($"{val} ");
+        }
+        Console.WriteLine();
+
+        var sol = new Solution();
+        var result = sol.Calculate(input);
+
+        Console.WriteLine($"Result: {result}");
+        Console.ReadKey();
     }
 }
--- OddOccurrencesInArray/Solution.cs
+++ OddOccurrencesInArray/Solution.cs
@@ -8,5 +8,25 @@ namespace OddOccurrencesInArray
 {
     internal class Solution
     {
+        public Solution() { }
+
+        public int Calculate(int[] A)
+        {
+            var occurences = new Dictionary<int, int>();
+
+            foreach (int val in A)
+            {
+                if (!occurences.ContainsKey(val))
+                {
+                    occurences[val] = 1;
+                }
+                else
+                {
+                    occurences.Remove(val);
+                }
+            }
+
+            return occurences.ElementAtOrDefault(0).Key;
+        }
     }
 }
