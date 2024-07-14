1812403a6a338cb5a318a8718049b22e5fc5ef3f
shiny13 <shahnawaz.alam13@gmail.com>
2024-07-14 14:33:04 +1000

    FrogRiverOne with solution

---
+++ FrogRiverOne/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FrogRiverOne;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("FrogRiverOne");
+
+        var X = 5;
+        var A = new int[] { 1, 3, 1, 4, 2, 3, 5, 6 };
+
+        Console.Write($"X: {X} | A: ");
+        foreach (var val in A)
+        {
+            Console.Write($"{val} ");
+        }
+        Console.WriteLine();
+
+        var sol = new Solution();
+        var result = sol.Calculate(X, A);
+
+        Console.WriteLine($"Position: {result}");
+        Console.ReadKey();
+    }
+}
---
+++ FrogRiverOne/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogRiverOne;
+
+internal class Solution
+{
+    public Solution() { }
+
+    public int Calculate(int X, int[] A)
+    {
+        int steps = X;
+        bool[] bitmap = new bool[steps + 1];
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (!bitmap[A[i]])
+            {
+                bitmap[A[i]] = true;
+                steps--;
+                if (steps == 0) return i;
+            }
+
+        }
+        return -1;
+    }
+}
