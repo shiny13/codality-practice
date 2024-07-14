88558c1351f6096b6eb05781b3534f82968f4782
shiny13 <shahnawaz.alam13@gmail.com>
2024-07-14 16:09:44 +1000

    StoneWall

---
+++ StoneWall/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace StoneWall;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ StoneWall/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneWall;
+
+internal class Solution
+{
+    public Solution() { }
+
+    public int Calculate(int[] H)
+    {
+        int result = 0;
+
+        var stack = new Stack<int>();
+        foreach (int newWallHeight in H)
+        {
+            while (stack.Count != 0)
+            {
+                int preStone = stack.Pop();
+                if (preStone == newWallHeight)
+                {
+                    stack.Push(preStone);
+                    break;
+                }
+                else if (newWallHeight > preStone)
+                {
+                    stack.Push(preStone);
+                    stack.Push(newWallHeight);
+                    break;
+                }
+                else
+                {
+                    result++;
+                }
+            }
+
+            if (stack.Count != 0)
+            {
+                stack.Push(newWallHeight);
+            }
+        }
+
+        return result + stack.Count;
+    }
+}
